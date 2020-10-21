d7414cf6fc2331030549e9215e83e996fcf2e0b2
arockyu <59586101+arockyu@users.noreply.github.com>
2020-10-21 18:06:06 +0900

    update

--- top.v
+++ top.v
@@ -11,5 +11,51 @@ module top (
     // set the on board LED continious on to indicate user program mode
     assign LED = 1;
 
+
+    
+    wire clk_double = CLK;
+
+    divider D1(
+        .clk(CLK),
+        .out(mclk));
+    defparam D1.divide_num=1;// N : Div parameter N+1 Divide
+
+
+    reg init = 1'b;
+    wire re,we;
+    wire [15:0] raddr,rdata,waddr,wdata;
+
+    //Test target module
+    COMET_II_top U101(
+        .mclk(mclk),
+        .init(init),
+        .rst(1'b0),
+        .PR_init(),
+        .SP_init(),
+        .re(re),
+        .raddr(raddr),
+        .rdata(rdata),
+        .we(we),
+        .waddr(waddr),
+        .wdata(wdata),
+        .stage()
+    );
+    defparam U101.initial_SP = 16'h0100;  //Force SP to 0100h for fit to test RAM size
+
+
+
+    //Main Program Area : 0000h - 004fh
+    //Sub-routin Area   : 0060h - 006fh
+    //data Area         : 0070h - 00B9h
+
+    Program_RAM R101(
+        .mclk(mclk),
+        .we(we),
+        .waddr(waddr),
+        .wdata(wdata),
+        .re(re),
+        .raddr(raddr),
+        .rdata(rdata) );
+
 endmodule
 
